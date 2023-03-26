5b73f408d6fb6b38d56c9f6f0d2bcd749d396094
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2023-03-27 02:51:59 +0530

    Add files via upload
    I have built the new folder locker application

---
+++ main/java/com/developerbhuwan/easyfolderlocker/folderlocker.java
@@ -0,0 +1,11 @@
+package com.developerbhuwan.easyfolderlocker;
+
+import com.developerbhuwan.easyfolderlocker.ui.MainUI;
+
+public class folderlocker {
+
+    public static void main(String[] args) {
+        new MainUI().setApp();       
+    }
+    
+}
